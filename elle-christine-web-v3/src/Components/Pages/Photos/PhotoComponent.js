75b6aa2689c32027adda62dc452cb8d119da9d52
RedBirdOBX <shane.fowlkes.70@gmail.com>
2024-12-16 14:10:40 -0500

    correcting img file name

--- elle-christine-web-v3/src/Components/Pages/Photos/PhotoComponent.js
+++ elle-christine-web-v3/src/Components/Pages/Photos/PhotoComponent.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import WOW from 'wowjs';
+
+// for the Wowjs library
+// https://stackoverflow.com/questions/70268322/how-to-use-wow-js-or-any-other-library-in-react
+
 
 const Photo = (props) =>
 {
@@ -6,12 +11,18 @@ const Photo = (props) =>
     const fileName = `imgs/content/gallery/${props.photo.fileName}`;
     const dataSubHtml = `<h4 class='text-white'>${props.photo.heading}</h4><p>${props.photo.description}</p>`;
 
+    useEffect(() => {
+        new WOW.WOW({
+          live: false
+        }).init();
+      }, [])
+
     return (
             <div className="col-md-6 col-lg-4 mt-1-9 wow fadeIn"
-                    data-wow-delay="100ms"
+                    data-wow-delay="1000ms"
                     data-src={fileName}
                     data-sub-html={dataSubHtml}>
-                <div class="portfolio-style1">
+                <div className="portfolio-style1">
                     <img src={imgPath}
                             className="border-radius-10"
                             alt={props.photo.description} />
